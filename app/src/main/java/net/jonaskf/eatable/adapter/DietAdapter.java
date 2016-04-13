817929dc050850e0a077699d45f811b75d2ccd3e
Jonas <mail@jonaskf.net>
2016-04-13 13:02:25 +0200

    fix for liste med dietter

--- app/src/main/java/net/jonaskf/eatable/adapter/DietAdapter.java
+++ app/src/main/java/net/jonaskf/eatable/adapter/DietAdapter.java
@@ -12,58 +12,63 @@ import android.widget.TextView;
 
 import net.jonaskf.eatable.R;
 import net.jonaskf.eatable.diet.Diet;
-import net.jonaskf.eatable.product.Product;
 
 /**
  * Created by jonas on 10.04.16.
  */
 public class DietAdapter  extends ArrayAdapter<Diet> {
-    private ImageButton imgBtn;
-    private Diet diet;
+    //private ImageButton imgBtn;
+
     public DietAdapter (Context context, int resource){
         super(context, resource);
     }
 
     @Override
-    public View getView(int pos, View convertView, ViewGroup parent){
+    public View getView(final int pos, View convertView, ViewGroup parent){
         View view = convertView;
+
         if(view == null){
             LayoutInflater viewInflater = LayoutInflater.from(getContext());
             view = viewInflater.inflate(R.layout.diet_list_row, null);
-            imgBtn = (ImageButton) view.findViewById(R.id.diet_list_row_btn);
+
         }
 
+        final ImageButton imgBtn = (ImageButton) view.findViewById(R.id.diet_list_row_btn);
         TextView nameTW = (TextView) view.findViewById(R.id.diet_row_name);
         //TextView companyTW = (TextView) view.findViewById(R.id.company_row_name);
 
-        diet = getItem(pos);
+        final Diet diet = getItem(pos);
+
+
 
         imgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("test", diet.getDiet());
-                addItem();
+                //Log.d("test", diet.getDiet() + " - ");
+                Log.d("test", v.getId()+ " - " );
+                addItem(diet, imgBtn);
             }
         });
-        btnIcon();
+
+        btnIcon(diet, imgBtn);
         if(nameTW != null)
             nameTW.setText(diet.getDiet());
         //if(companyTW != null)
         //companyTW.setText(product.getCompany);
-        Log.d("test", diet.getDiet());
+        Log.d("test", diet.getDiet()+"View running");
         return view;
     }
 
-    private void addItem(){
+    public void addItem(Diet diet, ImageButton imgBtn){
         if(!Diet.list.containsKey(diet.getId())){
             Diet.list.put(diet.getId(), diet);
         }else{
             Diet.list.remove(diet.getId());
         }
-        btnIcon();
+        btnIcon(diet, imgBtn);
     }
 
-    private void btnIcon(){
+    private void btnIcon(Diet diet, ImageButton imgBtn){
         if(Diet.list.containsKey(diet.getId())) {
             imgBtn.setImageResource(android.R.drawable.ic_menu_delete);
             imgBtn.setBackgroundColor(Color.RED);
